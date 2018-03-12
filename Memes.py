a3e3614584b7bd91701c14db65ab8bab7831931f
jordanaron22 <jordanaron22@gmail.com>
2018-03-12 16:00:51 -0500

    cleans and clusters

--- Memes.py
+++ Memes.py
@@ -12,6 +12,8 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 import copy
 import numpy as np
+import re
+from sklearn.cluster import KMeans
 
 def cleanData(data):
     data = data[~(data == 0).any(axis=1)]
@@ -25,15 +27,33 @@ def cleanData(data):
         for word_index in range(len(text_data[sentence_index])):
             if '\\n' in text_data[sentence_index][word_index]:
                 text_data[sentence_index][word_index] = text_data[sentence_index][word_index][:-2]
-            if "IG:" in text_data[sentence_index][word_index]:
-                text_data[sentence_index] = text_data[sentence_index][:word_index]
-                break
-            if word_index == len(text_data[sentence_index]) - 1:
-                if "@" in text_data[sentence_index][word_index]:
-                    text_data[sentence_index].remove(text_data[sentence_index][word_index])
+            # if "IG:" in text_data[sentence_index][word_index]:
+            #     text_data[sentence_index] = text_data[sentence_index][:word_index]
+            #     break
+            # if "@" in text_data[sentence_index][word_index]:
+            #     text_data[sentence_index].remove(text_data[sentence_index][word_index])
 
     for sentence_index in range(len(text_data)):
         text_data[sentence_index] = " ".join(text_data[sentence_index])
+
+
+    inds_to_remove = []
+    for sentence_ind in range(len(text_data)):
+        matchObj1 = re.search('.*([0-9]|[0-9][0-9]):[0-9][0-9].*',text_data[sentence_ind])
+        matchObj2 = re.search('[0-9]{5,}.*', text_data[sentence_ind])
+        matchObj3 = re.search('[0-9]+/[0-9]+/[0-9]+.*', text_data[sentence_ind])
+        if matchObj1 or matchObj2 or matchObj3:
+            inds_to_remove.append(sentence_ind)
+
+    inds_to_remove.reverse()
+
+    for ind in inds_to_remove:
+        del text_data[ind]
+        data.drop(data.index[ind],inplace=True)
+
+
+
+
 
     return [text_data,data]
 
@@ -54,6 +74,17 @@ def getSent(text_data):
 
     return sentiment_data
     #compound negative neutral positive
+
+
+def getTime(data):
+    date_list = []
+    for key, value in data.iterrows():
+        time = value[0].split()[1]
+        time = time.replace(":","")
+        date_list.append(time)
+
+    return date_list
+
 
 def normalizeScores(data):
     min_val = 0
@@ -123,37 +154,74 @@ def normalizeScores(data):
     return normalized_scores
 
 
-def qual2quant(normalized):
+def qual2quant(normalized, size):
     new_scores = []
     sorted_norm = copy.copy(normalized)
     sorted_norm.sort()
     max_length = len(sorted_norm)
-    bin_size = int(max_length/5)
-    first_20 = sorted_norm[bin_size]
-    second_20 = sorted_norm[bin_size*2]
-    third_20 = sorted_norm[bin_size*3]
-    fourth_20 = sorted_norm[bin_size*4]
 
+    if size == 2:
+        bin_size = int(max_length/2)
+        first_half = sorted_norm[bin_size]
+        for score in normalized:
+            if score < first_half:
+                new_scores.append(0)
+            else:
+                new_scores.append(1)
 
-
-    for score in normalized:
-        if score < first_20:
-            new_scores.append(0)
-        elif first_20 <= score < second_20:
-            new_scores.append(1)
-        elif second_20 <= score < third_20:
-            new_scores.append(2)
-        elif third_20 <= score < fourth_20:
-            new_scores.append(3)
-        elif fourth_20 <= score:
-            new_scores.append(4)
+    if size == 5:
+        bin_size = int(max_length/5)
+        first_20 = sorted_norm[bin_size]
+        second_20 = sorted_norm[bin_size*2]
+        third_20 = sorted_norm[bin_size*3]
+        fourth_20 = sorted_norm[bin_size*4]
+        for score in normalized:
+            if score < first_20:
+                new_scores.append(0)
+            elif first_20 <= score < second_20:
+                new_scores.append(1)
+            elif second_20 <= score < third_20:
+                new_scores.append(2)
+            elif third_20 <= score < fourth_20:
+                new_scores.append(3)
+            elif fourth_20 <= score:
+                new_scores.append(4)
 
     return new_scores
 
 
 
+def dataCombiner(sent_data, dates):
+    for sent_ind in range(len(sent_data)):
+        sent_data[sent_ind].append(dates[sent_ind])
+
+    return sent_data
 
 
+def cluster(sent_data, normalized):
+    compound = []
+    for sentiment in sent_data:
+        compound.append(sentiment[0])
+    df = pd.DataFrame({'x': compound, 'y': normalized})
+
+    kmeans = KMeans(n_clusters=2)
+    kmeans.fit(df)
+
+    labels = kmeans.predict(df)
+    centroids = kmeans.cluster_centers_
+
+    print(labels)
+    print(centroids)
+
+    fig = plt.figure(figsize=(5, 5))
+
+    plt.scatter(df['x'], df['y'], c=kmeans.labels_, cmap='rainbow')
+    plt.xlim(-1.05,1.05)
+    plt.ylim(-.05, 1.05)
+    plt.xlabel("Compound Sentiment Score")
+    plt.ylabel("Normalized Score")
+    plt.title("2 Means Clustering Donald Trump")
+    plt.show()
 
 
 
@@ -162,25 +230,23 @@ def qual2quant(normalized):
 
 def main():
 
-    sid = SentimentIntensityAnalyzer()
+    data = pd.read_csv('trump memes.csv', encoding="ISO-8859-1")
+    [cleaned_text,cleaned_data] = cleanData(data)
 
-    #data = pd.read_csv('Bern.csv', encoding="ISO-8859-1")
-    data = pd.read_csv('hillary memes.csv', encoding="ISO-8859-1")
-
-    cleaned_text = cleanData(data)[0]
-    cleaned_data = cleanData(data)[1]
-
+    getTime(cleaned_data)
     normalized = normalizeScores(cleaned_data)
-    normalized_bin = qual2quant(normalized)
+    normalized_bin = qual2quant(normalized,2)
 
     sent_data = getSent(cleaned_text)
+    dates = getTime(cleaned_data)
+
+    #cluster(sent_data,normalized)
 
 
-
-
+    combined_data = dataCombiner(sent_data,dates)
 
     model = Sequential()
-    model.add(Dense(12, input_dim=4, activation='softplus'))
+    model.add(Dense(12, input_dim=5, activation='softplus'))
     model.add(Dropout(0.5))
     model.add(Dense(12, activation='softplus'))
     model.add(Dropout(0.5))
@@ -188,7 +254,7 @@ def main():
     model.add(Dense(1, activation='hard_sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.fit(np.array(sent_data), np.array(normalized_bin), epochs=30, batch_size=20)
-    scores = model.evaluate(np.array(sent_data), np.array(normalized_bin))
+    scores = model.evaluate(np.array(combined_data), np.array(normalized_bin))
     print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
 
